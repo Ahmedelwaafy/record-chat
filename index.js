5041e7a6b1446f5c4146b54ba23276b4280ba4fe
Ahmed ElWafy <ahmedelwaafy@gmail.com>
2023-04-06 17:45:37 +0200

    edit3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const record = document.querySelector(".record-btn");
 const stop = document.querySelector(".stop-btn");
+const close = document.querySelector(".close-btn");
 const chatArea = document.querySelector(".chat-area");
 const input = document.querySelector(".input-text input");
 const uploadBtn = document.querySelector(".upload");
@@ -7,18 +8,66 @@ const sendBtn = document.querySelector(".send");
 const embedImgInput = document.querySelector("#embed-img");
 const chat = document.querySelector(".chat-icon");
 const chatFixed = document.querySelector(".chat-fixed");
+const recordArea = document.querySelector(".record");
+const inputArea = document.querySelector(".input-text");
+const uploadArea = document.querySelector(".upload-image");
+const timer = document.querySelector(".timer");
 
-
-
+let seconds = 1;
+let minutes = 0;
+let secondsWithZero;
+let minutesdWithZero;
+let counter;
+let showStopCloseBtns;
 let items = [];
 let recorder;
 let stream;
 
 function startRecording() {
   record.classList.toggle("hide");
-  stop.classList.toggle("hide");
+  
   input.setAttribute("disabled", "");
+  recordArea.style.width = "100%";
+  recordArea.style.backgroundColor = "#cba4f0";
+  inputArea.style.display = "none";
+  uploadArea.style.display = "none";
+  stop.classList.toggle("hide");
+  showStopCloseBtns=setTimeout(() => {
+    close.classList.toggle("hide");
+    timer.classList.toggle("hide");
 
+}, 1000);
+
+  //timer
+  counter = setInterval(() => {
+    seconds++;
+    if (seconds / 60 == 1) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 2) {
+        stopRecording();
+        clearInterval(counter);
+        seconds = 1;
+        minutes = 0;
+        timer.innerHTML ="00:01";
+        timer.classList.toggle("hide");
+      }
+    }
+    if (seconds < 10) {
+      secondsWithZero = "0" + seconds.toString();
+    } else {
+      secondsWithZero = seconds;
+    }
+
+    if (minutes < 10) {
+      minutesdWithZero = "0" + minutes.toString();
+    } else {
+      minutesdWithZero = minutes;
+    }
+    timer.innerHTML = minutesdWithZero + ":" + secondsWithZero;
+  }, 1000);
+
+  //recorder
   let device = navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false,
@@ -40,12 +89,28 @@ function stopRecording() {
   record.classList.toggle("hide");
   stop.classList.toggle("hide");
   input.removeAttribute("disabled");
+  close.classList.toggle("hide");
+  recordArea.style.width = "10%";
+  recordArea.style.backgroundColor = "transparent";
+  inputArea.style.display = "block";
+  uploadArea.style.display = "block";
+
+  //timer
+  clearInterval(counter);
+  seconds = 1;
+  minutes = 0;
+          timer.innerHTML = "00:01";
+
+  timer.classList.toggle("hide");
+
 
   recorder.stop();
 
   recorder.onstop = (e) => {
     let blob = new Blob(items, { type: "audio/webm" });
     items = [];
+    let audioUrl = URL.createObjectURL(blob);
+    console.log(audioUrl);
     newMessage = document.createElement("div");
     newMessage.classList.add("message");
     newMessage.innerHTML =
@@ -63,8 +128,35 @@ function stopRecording() {
   };
 }
 
+function closeRecording() {
+  record.classList.toggle("hide");
+  stop.classList.toggle("hide");
+  input.removeAttribute("disabled");
+  close.classList.toggle("hide");
+  recordArea.style.width = "10%";
+  recordArea.style.backgroundColor = "transparent";
+  inputArea.style.display = "block";
+  uploadArea.style.display = "block";
+
+  //timer
+  clearInterval(counter);
+  seconds = 1;
+  minutes = 0;
+          timer.innerHTML = "00:01";
+
+  timer.classList.toggle("hide");
+
+  recorder.stop();
+  recorder.onstop = (e) => {
+    items = [];
+    stream.getAudioTracks().forEach((track) => track.stop());
+    stream = null;
+  };
+}
+
 record.addEventListener("click", startRecording);
 stop.addEventListener("click", stopRecording);
+close.addEventListener("click", closeRecording);
 
 uploadBtn.addEventListener("click", () => {
   record.classList.add("hide");
@@ -90,14 +182,15 @@ input.addEventListener("keyup", (e) => toggleRecordSend(e));
 function sendTextOrImage() {
   let text = input.value;
   let imgFiles = embedImgInput.files;
- 
 
   if (text !== "") {
     textMessage = document.createElement("div");
     textMessage.classList.add("text-message");
     textMessage.innerHTML =
       `
-                    <p >` +text+`</p>
+                    <p >` +
+      text +
+      `</p>
      
                     <div class="user-img">
                         <img src="./user.svg" >
@@ -108,7 +201,7 @@ function sendTextOrImage() {
   }
 
   if (imgFiles.length > 0) {
-     let imgsrc = URL.createObjectURL(embedImgInput.files[0]);
+    let imgsrc = URL.createObjectURL(embedImgInput.files[0]);
     imgMessage = document.createElement("div");
     imgMessage.classList.add("img-message");
     imgMessage.innerHTML =
@@ -123,7 +216,6 @@ function sendTextOrImage() {
 `;
     chatArea.appendChild(imgMessage);
   }
-  
 }
 
 sendBtn.addEventListener("click", sendTextOrImage);
